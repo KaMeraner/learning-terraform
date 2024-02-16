651e3584c3fd8ba5a643bed01da35321ff1aa7f1
KaMeraner <160024873+KaMeraner@users.noreply.github.com>
2024-02-16 13:19:54 +0100

    Fixing vpc id

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_instance" "blog" {
 resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow https/https in. Allow everything out."
-  vpc_id      = data.aws_vpc.vpc_id
+  vpc_id      = data.aws_vpc.default.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
