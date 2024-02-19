b3446d695a0e791b7eac239ab78a8b19939b6aab
KaMeraner <160024873+KaMeraner@users.noreply.github.com>
2024-02-19 14:18:33 +0100

    test with .arn

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ module "blog_asg" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.alb_target_group_arn
+  target_group_arns   = module.blog_alb.arn
   security_groups     = [module.blog_sg.security_group_id]
 
   image_id        = data.aws_ami.app_ami.id
