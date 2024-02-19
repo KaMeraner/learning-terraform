b09758465c955a1b35a4020597825bb68ccad7e8
KaMeraner <160024873+KaMeraner@users.noreply.github.com>
2024-02-19 14:06:17 +0100

    try to fix target_group_arns

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ module "asg" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
+  target_group_arns   = module.blog_alb.target_group.arn
   security_groups     = [module.blog_sg.security_group_id]
 
   image_id        = data.aws_ami.app_ami.id
@@ -66,7 +66,6 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
     }
   }
   listeners = {
