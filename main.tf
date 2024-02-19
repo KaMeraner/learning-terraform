1c68afeb83763059d9a26bf654b9fd4006b995c9
KaMeraner <160024873+KaMeraner@users.noreply.github.com>
2024-02-19 13:16:03 +0100

    create new dev vpc

--- main.tf
+++ main.tf
@@ -18,11 +18,27 @@ data "aws_vpc" "default" {
   default = true
 }
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "HelloWorld"
@@ -34,10 +50,11 @@ module "blog_sg" {
   version = "5.1.0"
   name    = "blog_new"
 
-  vpc_id  = data.aws_vpc.default.id
+  vpc_id  = module.blog_vpc.vpc_id
+  name = "blog"
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["93.104.65.206/32"]
+  ingress_cidr_blocks = ["93.104.84.218/32"]
 
   egress_rules        = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
